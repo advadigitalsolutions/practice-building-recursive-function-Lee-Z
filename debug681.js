05fd1c2338d9c333d53b8e8001320a13309cfc6d
Lee Zuvanich <leezuvanich@Levis-MacBook-Air.local>
2025-04-17 09:36:11 -0700

    finished lesson

--- debug681.js
+++ debug681.js
@@ -1,7 +1,8 @@
+/*
 let launchReady = false;
 let fuelLevel = 17000;
 
-if (fuelLevel >= 20000 {
+if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
    launchReady = true;
 } else {
@@ -9,5 +10,89 @@ if (fuelLevel >= 20000 {
    launchReady = false;
 }
 
+//let launchReady = false; - hiding as this has already been declared
+let crewStatus = true;
+let computerStatus = 'green';
 
+if (crewStatus && computerStatus === 'green') {
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+
+if (launchReady) {
+   console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+   console.log("Fed parrot...");
+   console.log("Ignition...");
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed.");
+}
+
+
+
+let launchReady = false;
+let fuelLevel = 17000;
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+
+let launchReady = false;
+let fuelLevel = 27000;
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+
+if (launchReady) {
+   console.log("10, 9, 8...");
+   console.log("Fed parrot...");
+   console.log("6, 5, 4...");
+   console.log("Ignition...");
+   console.log("3, 2, 1...");
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed.");
+}
+
+let launchReady = false;
+let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+
+if (crewStatus && computerStatus === 'green' && launchReady){
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
+
+*/
 
