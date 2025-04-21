63a0018fa0380ca0e40e7179872594c9c1b7c7a5
Lee Zuvanich <leezuvanich@Levis-MacBook-Air.local>
2025-04-20 23:02:16 -0700

    finished assignment

---
+++ recursive1.js
@@ -0,0 +1,11 @@
+function reverseString(str) {
+    if (str.length <= 1) {
+        return str;
+    }
+    return str[str.length -1] + reverseString(str.slice(0, -1));
+}
+
+console.log(reverseString("hello"));
+console.log(reverseString("recursion"));
+console.log(reverseString("a"));
+console.log(reverseString(""));
